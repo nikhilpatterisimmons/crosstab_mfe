026588fb0543f0affcd95e536d6f2464deeb026a
Nikhil Patteri <nikhilpatteri@gmail.com>
2022-03-14 16:10:42 +0530

    first commit1

---
+++ src/components/Crosstab.tsx
@@ -0,0 +1,9 @@
+import crosstabImg from '../assets/crosstab.png';
+
+const Crosstab = () => {
+  return (<div>
+    <img style={{ maxWidth: '100%', maxHeight: '100%' }} src={crosstabImg} alt="Crosstab image" />
+  </div>)
+}
+
+export default Crosstab;
---
+++ src/root.component.tsx
@@ -0,0 +1,5 @@
+import Crosstab from "./components/Crosstab";
+
+export default function Root(props) {
+  return <Crosstab />;
+}
